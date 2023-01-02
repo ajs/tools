3f387b009dda7389c217abc2b81bbfc0805b48ec
Aaron Sherman <ajs@ajs.com>
2023-01-02 10:58:42 -0500

    Command-line args

--- games/df/dfhack-scripts/show-imposters.lua
+++ games/df/dfhack-scripts/show-imposters.lua
@@ -54,6 +54,10 @@ local function titlecase(str)
     return str:gsub("(%a)(%a+)", function(a, b) return string.upper(a) .. string.lower(b) end)
 end
 
+argparse = require('argparse')
+utils = require('utils')
+
+local args = argparse.processArgs({...}, utils.invert{'active', 'vampire'})
 local imp_count = 0
 for _,unit in ipairs(df.global.world.units.all) do
     local alias = dfhack.units.getIdentity(unit)
@@ -74,56 +78,59 @@ for _,unit in ipairs(df.global.world.units.all) do
         local age = round(dfhack.units.getAge(unit), 1)
         local real_age = round(dfhack.units.getAge(unit, true), 1)
 
-	imp_count = imp_count + 1
+        if (not args.active or active) and (not args.vampire or bloodsucker) then
 
-        print("** Imposter found! Current alias: "..alias_name.." ("..race_name..")")
-        print("   - "..real_name.." is their real name.")
-        if not historical_name == real_name and not historical_name == alias_name then
-            print("   - "..historical_name.." is their historical name.")
+            imp_count = imp_count + 1
+
+            print("** Imposter found! Current alias: "..alias_name.." ("..race_name..")")
+            print("   - "..real_name.." is their real name.")
+            if not historical_name == real_name and not historical_name == alias_name then
+                print("   - "..historical_name.." is their historical name.")
+            end
+            if not soul_name == real_name and not soul_name == alias_name then
+                print("   - "..soul_name.." is their soul name.")
+            end
+            print("   - Additionally:")
+            local prefix = "   - * "
+            if nemesis and not nemesis.unit_id == unit.id then
+                local nemesis_unit = df.unit.find(nemesis.unit_id)
+                local nemesis_name = dfhack.TranslateName(dfhack.units.getVisibleName(nemesis_unit))
+                print(prefix.."has a nemesis: "..nemesis_name)
+            end
+            if hidden_curse then
+                print(prefix.."hides the nature of their curse")
+            end
+            if crazed then
+                print(prefix.."crazed")
+            end
+            if anti_life then
+                print(prefix.."opposed to life")
+            end
+            if bloodsucker then
+                print(prefix.."drinks blood")
+            end
+            if not active then
+                print(prefix.."inactive")
+            end
+            if alive then
+                print(prefix.."alive")
+            else
+                print(prefix.."dead ('your friend is only mostly dead')")
+            end
+            local citizen_status = "visitor"
+            if citizen then
+                citizen_status = "citizen"
+            end
+            print(prefix..citizen_status)
+            local age_tag = "age"
+            local age_value = age
+            if not age == real_age then
+                age_tag = "age (real)"
+                age_value = age.." ("..real_age..")"
+            end
+            print(prefix..age_tag..": "..age_value)
+            print(" ")
         end
-        if not soul_name == real_name and not soul_name == alias_name then
-            print("   - "..soul_name.." is their soul name.")
-        end
-        print("   - Additionally:")
-        local prefix = "   - * "
-        if nemesis and not nemesis.unit_id == unit.id then
-            local nemesis_unit = df.unit.find(nemesis.unit_id)
-            local nemesis_name = dfhack.TranslateName(dfhack.units.getVisibleName(nemesis_unit))
-            print(prefix.."has a nemesis: "..nemesis_name)
-        end
-        if hidden_curse then
-            print(prefix.."hides the nature of their curse")
-        end
-        if crazed then
-            print(prefix.."crazed")
-        end
-        if anti_life then
-            print(prefix.."opposed to life")
-        end
-        if bloodsucker then
-            print(prefix.."drinks blood")
-        end
-        if not active then
-            print(prefix.."inactive")
-        end
-        if alive then
-            print(prefix.."alive")
-        else
-            print(prefix.."dead ('your friend is only mostly dead')")
-        end
-        local citizen_status = "visitor"
-        if citizen then
-            citizen_status = "citizen"
-        end
-        print(prefix..citizen_status)
-        local age_tag = "age"
-        local age_value = age
-        if not age == real_age then
-            age_tag = "age (real)"
-            age_value = age.." ("..real_age..")"
-        end
-        print(prefix..age_tag..": "..age_value)
-        print(" ")
 
     end
 end
@@ -131,3 +138,4 @@ end
 if imp_count > 0 then
     print("Total imposters: "..imp_count)
 end
+
